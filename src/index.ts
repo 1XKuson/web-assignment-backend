a2a60cbe4c57bc248c56e8dc9927f48dbd2cc553
Kuson Ta <115791113+1XKuson@users.noreply.github.com>
2025-03-13 18:59:55 +0700

    Enhance CORS configuration to specify allowed origins, methods, and headers

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+    cors({
+      origin: "*", 
+      methods: ["GET", "POST"], 
+      allowedHeaders: ["Content-Type", "Authorization"], 
+    })
+  );
 app.use(express.json());
 
 // Server 1: Drone Config Server
